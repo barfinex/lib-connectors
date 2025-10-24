7c69319baee0667406e76cc29032dbb50090deba
barfinex-ci <ci@barfinex.com>
2025-10-24 06:27:20 +0000

    fix(connectors): trigger release

--- src/datasource/binance/history.d.ts
+++
@@ -1,4 +0,0 @@
-import { TimeFrame, CandleRaw, MarketType } from '@barfinex/types';
-export declare function createRequestBinance(marketType: MarketType): (from: number, to: number, symbolName: string, interval: TimeFrame, opts?: {
-    maxBars?: number;
-}) => Promise<CandleRaw[]>;
--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './connector.module';
-export * from './connector.service';
-export { createRequestBinance } from './datasource/binance/history';
-export { requestAlpaca } from './datasource/alpaca/history';
